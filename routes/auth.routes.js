c5073c0240eaaffeb3df1ef3745982876a5ecf0f
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2020-06-04 17:38:45 +0300

    added registration validation with express-validator middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,32 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
-
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
-
 const router = Router()
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+  '/register',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', 'Password length should be more than 6 symbols')
+      .isLength({ min: 6 })
+  ],
+  async (req, res) => {
   try {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Incorrect registration data'
+      })
+    }
     
     const {email, password} = req.body
 
     const candidate = User.findOne({ email })
     if(candidate) {
-      res.status(400).json({ message: 'Such user already exist' })
+      return res.status(400).json({ message: 'Such user already exist' })
     }
 
     const hashedPassword = bcrypt.hash(password, 12)
