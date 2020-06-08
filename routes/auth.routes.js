c81704b6b05ae09623ef5d19a30883dc8e96f205
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2020-06-08 18:52:26 +0300

    login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
@@ -24,12 +26,12 @@ router.post(
     
     const {email, password} = req.body
 
-    const candidate = User.findOne({ email })
+    const candidate = await User.findOne({ email })
     if(candidate) {
       return res.status(400).json({ message: 'Such user already exist' })
     }
 
-    const hashedPassword = bcrypt.hash(password, 12)
+    const hashedPassword = await bcrypt.hash(password, 12)
 
     const user = new User({ email, password: hashedPassword })
     await user.save()
@@ -42,7 +44,46 @@ router.post(
 })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+  '/login',
+  [
+    check('email', 'Enter valid email, please').normalizeEmail().isEmpty(),
+    check('password', 'Password should be not empty').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if(!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect login data'
+        })
+      }
+      
+      const {email, password} = req.body
+
+      const user = await User.findOne({ email })
+      if(user) {
+        return res.status(400).json({ message: 'User not found' })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if(!isMatch) {
+        return res.status(400).json({ message: 'Password do not match, please try again' })
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      )
+
+      res.json({ token, userId: user.id })
+  
+    } catch (error) {
+      res.status(500).json({ message: 'Something has happened on the server' })
+    }
 
 })
 
