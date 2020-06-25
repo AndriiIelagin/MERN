d59518f9059e087a08c834caaeba0c3e3bd7c1b5
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2020-06-25 19:06:33 +0300

    auth hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import 'materialize-css'
 import { BrowserRouter as Router } from 'react-router-dom'
+
 import { useRoutes } from './routes'
+import { useAuth } from './hooks/auth.hook'
 
 function App() {
+  const {login, logout, token, userId} = useAuth
   const routes = useRoutes(false)
+
   return (
     <Router>
       <div className="container">
