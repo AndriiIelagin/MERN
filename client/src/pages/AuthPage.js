022845c8d42e252d1f43521f6f1f23a90b5400b8
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2020-06-25 19:19:15 +0300

    auth context

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 
 import { useHttp } from '../hooks/http.hook'
 import { useMessage } from '../hooks/message.hook'
+import { AuthContext } from '../context/AuthContext'
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext)
   const message = useMessage()
   const [form, setForm] = useState({
     email: '', password: ''
@@ -29,7 +31,7 @@ export const AuthPage = () => {
   const loginHandler = async () => {
     try {
       const data = await request('/api/auth/login', 'POST', {...form})
-      message(data.message)
+      auth.login(data.token, data.userId)
     } catch (e) {}
   }
 
@@ -51,7 +53,7 @@ export const AuthPage = () => {
                   className="yellow-input"
                   onChange={changeHandler}
                 />
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
               </div>
               <div className="input-field">
                 <input 
